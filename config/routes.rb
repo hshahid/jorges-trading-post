99fcb6ecbc7349f34b490addf118589b901ce081
Hashma <shahi22h@mtholyoke.edu>
2015-10-04 22:07:58 -0400

    cleaned code

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
 
-  root ‘listings#index'
+  root 'listings#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
