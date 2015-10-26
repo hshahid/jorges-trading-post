6b1bfa93a81c7220ec0ed406da0a2d717a406e7f
Hashma <shahi22h@mtholyoke.edu>
2015-10-26 01:04:47 -0400

    more validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,13 @@ class Listing < ActiveRecord::Base
 		  	:storage => :dropbox,
       		:dropbox_credentials => Rails.root.join("config/dropbox.yml")
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+  #check to see whether all the fields have been filled
+  validates :name, :description, :price, presence: true
+
+  #make sure that the price field is popilated by a number greater than 0
+  validates :price, numericality: { greater_than: 0 }
+
+  #validate image
+  validates_attachment_presence :image
 end
