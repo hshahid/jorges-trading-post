01359839d22ee16e02d289ef2a1763925e590b4d
Hashma <shahi22h@mtholyoke.edu>
2015-11-23 08:14:04 -0500

    order form

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,6 +15,7 @@ class OrdersController < ApplicationController
   # GET /orders/new
   def new
     @order = Order.new
+    @listing = Listing.find(params[:listing_id])
   end
 
   # GET /orders/1/edit
@@ -25,11 +26,16 @@ class OrdersController < ApplicationController
   # POST /orders.json
   def create
     @order = Order.new(order_params)
+    @listing = Listing.find(params[:listing_id])
+    @seller = @listing.user
+
+    @order.listing_id = @listing.id
     @order.buyer_id = current_user.id
+    @order.seller_id = @seller_id
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
+        format.html { redirect_to root_url, notice: 'Order was successfully created.' }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
